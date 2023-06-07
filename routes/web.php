aef91069a77c696b1de1a969a897cada1897be06
Dmytro Zelonkin <dmytro.zelonkin@sigma.software>
2023-06-07 16:46:36 +0300

    Added code with security issue

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users/{id}', function (int $id) {
+    $user = \App\Models\User::where('id', $id)->first();
+
+    dd($user);
+});
