c8a6c1320ad83096d61bc12a2d08494201d74cf7
Dmytro Zelonkin <dmytro.zelonkin@sigma.software>
2023-06-30 16:34:36 +0300

    wip

---
+++ resources/js/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="one" id="1">
+    <h1>Hello world</h1>
+    <p>Some description</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'App',
+
+  computed: {},
+};
+</script>
