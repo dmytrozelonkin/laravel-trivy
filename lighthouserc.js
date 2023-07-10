aa52e75ed9b17953392e440127e22bb221633543
Dmytro Zelonkin <dmytro.zelonkin@sigma.software>
2023-07-10 17:35:42 +0300

    Added lighthouserc.js

---
+++ lighthouserc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    ci: {
+        collect: {
+            url: ['https://sigma.software/'],
+        },
+        upload: {
+            target: 'temporary-public-storage',
+        },
+    },
+};
